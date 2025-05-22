b80a9d246c807a2a1c4749616777ec2bd675d3af
Athanasopoulos Nontas <ninjanody@hotmail.gr>
2025-05-22 03:05:21 +0300

    Add files via upload

---
+++ database/migrations/2021_08_08_184838_create_pickups_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePickupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pickups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+			$table->integer('passengers');
+            $table->foreignId('driver_id')->constrained();
+            $table->foreignId('location_id')->constrained();
+            $table->string('status')->default('called');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pickups');
+    }
+}
