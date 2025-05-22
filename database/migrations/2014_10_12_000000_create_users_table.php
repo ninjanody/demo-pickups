0e74a0ef8a0251f9220887dfec8c8ed71060663a
Athanasopoulos Nontas <ninjanody@hotmail.gr>
2025-05-22 11:30:36 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address')->nullable();
             $table->string('phone_number')->nullable();
             $table->boolean('terms_accepted');
             $table->rememberToken();
